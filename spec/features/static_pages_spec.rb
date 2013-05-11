0a6e0521753567942c747271c1a3d9fa4b679002
Victor Ryadovoi <r.v.i.c@yandex.ru>
2013-05-11 15:03:22 +0600

    Add test for second static page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,9 +4,15 @@ describe "Static Pages" do
   describe "Home page" do
     it "should have content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit 'static_pages/home'
+      visit '/static_pages/home'
       page.should have_content('Sample App')
       #response.status.should be(200)
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
     end
   end
 end
