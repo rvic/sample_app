d069658544accd67e1f24c8937da9ba9d79cbb1a
Victor Ryadovoi <r.v.i.c@yandex.ru>
2013-05-11 14:42:52 +0600

    Fix Capybara issue for visit method'

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+    it "should have content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit 'static_pages/home'
+      page.should have_content('Sample App')
+      #response.status.should be(200)
+    end
+  end
+end
