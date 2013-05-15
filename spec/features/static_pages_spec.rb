8725b4388121ae2e8e7bc097dcf8071f78433c2a
Victor Ryadovoi <r.v.i.c@yandex.ru>
2013-05-15 15:35:50 +0600

    Add a helper for title

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,21 +2,25 @@ require 'spec_helper'
 
 describe "Static Pages" do
   describe "Home page" do
-    it "should have content 'Sample App'" do
+    it "should have h1 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       #page.should have_content('Sample App')
       page.should have_selector('h1', :text => 'Sample App')
       #response.status.should be(200)
     end
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
       #page.source.should have_xpath('//title[contains(.,"Ruby on Rails Tutorial Sample App | Home")]')
-      page.should have_title("Home")
+      page.should have_title("Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_title("| Home")
     end
   end
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
       #page.should have_content('Help')
       page.should have_selector('h1', :text => 'Help')
@@ -28,7 +32,7 @@ describe "Static Pages" do
     end
   end
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
       #page.should have_content('About Us')
       page.should have_selector('h1', :text => 'About Us')
@@ -40,7 +44,7 @@ describe "Static Pages" do
     end
   end
   describe "Contact page" do
-    it "should have the content 'Contact'" do
+    it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
       #page.should have_content('About Us')
       page.should have_selector('h1', :text => 'Contact')
